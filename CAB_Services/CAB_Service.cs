00830081b7c7f22e07c6c282caab4e18f70a5b47
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-30 20:34:18 +0530

    Welcome Message added

---
+++ CAB_Services/CAB_Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CAB_Services
+{
+    class CAB_Service
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To CAB Service Center");
+        }
+    
+    }
+    
+}
+
+
